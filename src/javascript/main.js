9a734478520603bcb73cb046a685742fcc78234d
Zilonis123 <kazaks.karlis2009@gmail.com>
2022-12-24 12:56:54 +0200

    test

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -179,7 +179,9 @@ async function reload() {
   removeChildren("launches");
 
   const data = await getData();
+  const test = document.getElementById("test");
   data.forEach(element => {
+    test.innerHTML += element
     makeCard({
       "img": element.image, videoURL: element.vidURLs[0],
       "description": element.mission ? element.mission.description : "",
